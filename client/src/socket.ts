639d124da78f1221b19ba6f27c2fe86d37f136b6
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2026-02-05 13:01:35 +0100

    fix: bug lucas

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 // GameMaster
-export const API_URL = "http://192.168.10.1:3000"; // ESD MAC
+// export const API_URL = "http://192.168.10.1:3000"; // ESD MAC
 // export const API_URL = "http://192.168.1.46:3000"; // THOMAS HOME
-// export const API_URL = "http://10.14.73.40:3000"; // THOMAS ESD
+export const API_URL = "http://10.14.73.40:3000"; // THOMAS ESD
 
 export const socket = io(API_URL);
